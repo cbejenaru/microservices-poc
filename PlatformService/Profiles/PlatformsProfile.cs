e0f01cb10aaa9b37260941e6fc46810bce61ebb4
Constantin Bejenaru <constantin.bejenaru@stefanini.com>
2022-12-20 16:13:06 +0200

    [feat] Add AutoMapper, add controller.

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles
+{
+    public class PlatformsProfile : Profile
+    {
+        public PlatformsProfile()
+        {
+            //Source -> Target
+            CreateMap<Platform, PlatformReadDto>();
+            //Target -> Source
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
